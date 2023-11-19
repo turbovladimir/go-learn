1e936c7d2c15b68d0bdc2a7be72116c7b56db852
sadovnikov <vladimir.sadovnikov@megamarket.ru>
2023-11-19 16:42:57 +0300

    4. print how hours and mins get from degrees of curcle

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 
 func main() {
 
-	var a int
-	fmt.Scan(&a) // считаем переменную 'a' с консоли
+	var degrees int
+	fmt.Scan(&degrees) // считаем переменную 'a' с консоли
 
-	fmt.Println((a / 10) % 10)
+	hours := degrees / 30
+	mins := 2 * (degrees % 30)
+	message := fmt.Sprintf("It is %d hours %d minutes.", hours, mins)
+
+	fmt.Println(message)
 }
