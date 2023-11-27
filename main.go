c641c3e7b1954cee358f16baf4d38b690e49f4ea
sadovnikov <vladimir.sadovnikov@megamarket.ru>
2023-11-27 12:13:46 +0300

    7. print sum of 2 digits numbers which division without remainder on 8

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, s int
-	fmt.Scan(&a)
-	fmt.Scan(&b)
+	var n, s, a int
 
-	for a <= b {
-		s += a
-		a++
+	fmt.Scan(&n)
+	arr := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+		a = arr[i]
+
+		if a/10 < 10 && a/10 > 0 && a%8 == 0 {
+			s += a
+		}
 	}
 
 	println(s)
