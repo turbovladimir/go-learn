7271dd754e99026868c136f443f6cbe3cbbf3c95
sadovnikov <vladimir.sadovnikov@megamarket.ru>
2023-11-27 09:22:08 +0300

    7. print square of asc ints

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var input int
-	fmt.Scanln(&input)
-	var a []int
 
-	for input > 0 {
-		v := input % 10
-		a = append(a, v)
-		input /= 10
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i * i)
 	}
-
-	if len(a) != 6 {
-		panic("Ticket id must contains 6 digits")
-	}
-
-	if a[0]+a[1]+a[2] == a[3]+a[4]+a[5] {
-		fmt.Println("YES")
-
-		return
-	}
-
-	fmt.Println("NO")
 }
