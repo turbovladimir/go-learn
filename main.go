b21362c00001d00788c7530c70e9c21a11cc00f2
sadovnikov <vladimir.sadovnikov@megamarket.ru>
2023-11-27 13:37:04 +0300

    7. working with cache by using map

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var n, s, a int
+	//var a int
+	m := make(map[int]int)
+	c := 0
 
-	fmt.Scan(&n)
-	arr := make([]int, n)
+	for i := 0; i < 10; i++ {
+		fmt.Scan(&c)
 
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i])
-		a = arr[i]
+		v, ok := m[c]
 
-		if a/10 < 10 && a/10 > 0 && a%8 == 0 {
-			s += a
+		if !ok {
+			v = work(c)
+			m[c] = v
 		}
-	}
 
-	println(s)
+		fmt.Print(v)
+		fmt.Print(" ")
+	}
+}
+func work(i int) int {
+	return i
 }
